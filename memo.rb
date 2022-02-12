6c50f99399089a1a7025a5c44fc060e198f14bd6
EC2 Default User <ec2-user@ip-172-31-95-82.ec2.internal>
2022-02-12 13:56:31 +0000

    Ruby課題ーメモアプリ１回目

---
+++ memo.rb
@@ -0,0 +1,35 @@
+require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+memo_type = gets.to_i
+
+if memo_type == 1
+    puts "拡張子を除いたファイルを入力してください"
+    file_name = gets.to_s
+    
+    puts "メモしたい内容を記入してください"
+    puts "完了したら Ctrl+D を押してください"
+    
+    file_text = STDIN.read
+    
+    CSV.open("#{file_name}.csv", "w") do |csv|
+     csv.puts ["#{file_text}"]
+    end
+end
+
+if memo_type == 2
+    puts "編集したいファイルについて、拡張子を除いたファイル名を入力してください"
+    file_name = gets.to_s
+    
+    puts "指定した以下のCSVファイルについて、編集してください"
+    puts CSV.read("#{file_name}.csv")
+    puts "編集が完了したら Ctrl+D を押します"
+    
+    file_text = STDIN.read
+    
+    CSV.open("#{file_name}.csv", "w") do |csv|
+     csv.puts ["#{file_text}"]
+    end
+end
+
